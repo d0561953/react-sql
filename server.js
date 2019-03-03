69f314aeb9ec310bd62164a45c3fa98308f64e63
Charles_Sin <charlessin1993@gmail.com>
2019-03-03 12:00:37 +0800

    unzip file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mysql = require('mysql');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+const connection = mysql.createConnection({
+   host: `localhost`,
+   user: `root`,
+   password: `root`,
+   database: `world`
+});
+
+connection.connect(function(err){
+   (err)? console.log(err): 
+   console.log(connection);
+});
+
+require('./routes/html-routes')(app, connection);
+
+app.listen(PORT, () => {
+   console.log(`App running on port ${PORT}`);
+});
